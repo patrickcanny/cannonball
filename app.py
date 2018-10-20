3be7cc919ee04e36c90eec4a4cdb33386be81aa4
marshingjay <jam605@gmail.com>
2018-10-19 22:04:20 -0500

    added mongodb setup, test insertion

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 # Cannonball Internal API
 # IMPORTS
 import os
+import urllib
 from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
 from flask_pymongo import PyMongo
+
 # END IMPORTS
 
 # Define App requirements
 app = Flask(__name__, static_folder="../static", template_folder="../static")
 
 # CONFIG
-app.config["MONGO_URI"] = "TODO"
+app.config["MONGO_URI"] = "mongodb://" + urllib.parse.quote("marshingjay@ku.edu") + ":" + urllib.parse.quote("Sportingbv#9") + "@cluster0-shard-00-00-pevs9.gcp.mongodb.net:27017,cluster0-shard-00-01-pevs9.gcp.mongodb.net:27017,cluster0-shard-00-02-pevs9.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
 app.debug=True
+mongo = PyMongo(app)
 
 @app.route("/test", methods=['GET'])
 def test():
+    mongo.db.users.insert({"name": "jacob" })
     app.logger.debug("Got Request from Flutter")
     return 'Nice! You Triggered it!'
 
