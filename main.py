3452d8b8e41ef7d58b7c4bcd225e8a7d812c94aa
Patrick Canny <patrickcanny@ku.edu>
2018-10-20 17:47:57 -0500

    export to slack

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import os
 import urllib
 import json
 from bson import Binary, Code
-from bson.json_util import dumps
+from bson.json_util import dumps, loads
 from bson.objectid import ObjectId
 from flask import Flask, render_template, request, flash, redirect, url_for, session, logging, jsonify
 from flask_pymongo import PyMongo
+from slackclient import SlackClient
 # END IMPORTS
 
 # HERE API
@@ -18,6 +19,9 @@ here_baseURL = "https://pos.api.here.com/positioning/v1/"
 
 # Define App requirements
 app = Flask(__name__, static_folder="../static", template_folder="../static")
+
+# SLACK API
+slack_client = SlackClient("xoxp-460967073045-460768312130-461837855431-1d2fa2229b2ae01e5d78e7279be3a66b")
 
 # CONFIG
 app.config["MONGO_URI"] = "mongodb://" + urllib.parse.quote("cannonball") + ":" + urllib.parse.quote("test") + "@cluster0-shard-00-00-pevs9.gcp.mongodb.net:27017,cluster0-shard-00-01-pevs9.gcp.mongodb.net:27017,cluster0-shard-00-02-pevs9.gcp.mongodb.net:27017/CannonballDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
@@ -62,6 +66,28 @@ def insertNewUser():
 @app.route("/newGroup", methods=['POST'])
 def insertNewGroup():
     newGroup = request.get_json()
+    app.logger.debug(newGroup)
+    newusers = []
+    newadmins = []
+    for user in newGroup.get('users'):
+        uid = str(user['$oid'])
+        obj = ObjectId(uid)
+        newusers.append(obj)
+
+    for user in newGroup.get('admins'):
+        uid = str(user['$oid'])
+        obj = ObjectId(uid)
+        newadmins.append(obj)
+
+    creator = newGroup.get('creator')
+    uid = str(user['$oid'])
+    obj = ObjectId(uid)
+    newcreator = obj
+
+    app.logger.debug(newusers)
+    newGroup['users'] = newusers
+    newGroup['admins'] = newadmins
+    newGroup['creator'] = newcreator
     app.logger.info(newGroup)
     mongo.db.groups.insert(newGroup)
     return dumps(newGroup)
@@ -70,19 +96,24 @@ def insertNewGroup():
 def insertNewEvent():
     newEvent = request.get_json()
     name = newEvent.get('name')
-    groupId = newEvent.get('groupid')
-    app.logger.info(groupId)
+    app.logger.info(newEvent)
+    gid = str(newEvent.get('groupid')['$oid'])
+    app.logger.info(gid)
+    newEvent['groupid'] = ObjectId(gid)
     app.logger.info(newEvent)
     mongo.db.events.insert(newEvent)
-    event = mongo.db.events.find_one({'name': name})
-    eid = event.get('_id')
-    group = mongo.db.groups.find_one({'_id': ObjectId(groupId)})
-    app.logger.info(group)
+    try:
+        event = mongo.db.events.find_one({'name': name})
+        eid = event.get('_id')
+    except:
+        return "Could not find newly created event. Perhaps your group does not exist"
+    try:
+        group = mongo.db.groups.find_one({'_id': ObjectId(gid)})
+    except:
+        return "Group not found, please create a new group first."
     groupEvents = group.get('events')
     if eid not in groupEvents:
-        mongo.db.groups.update_one({'_id': ObjectId(groupId)},{'$push':{'events':eid}})
-        app.logger.info("Added event to group {}", gid)
-
+        mongo.db.groups.update_one({'_id': ObjectId(gid)},{'$push':{'events':eid}})
     return dumps(newEvent)
 
 @app.route("/checkInUser", methods=['POST'])
@@ -96,23 +127,23 @@ def checkInUser():
     targetUser = mongo.db.users.find_one({'email': useremail})
     targetGroup = targetEvent.get('groupid')
     groups = targetUser.get('groups')
-    found = False
-    for x in groups:
-        if x == targetGroup :
-            found = True
-            break
-    if found == False :
-        app.logger.info("FALSE")
+
+    if targetGroup not in groups:
         myId = targetUser.get('_id')
         mongo.db.users.update_one({'_id': myId}, {'$push': {'groups': targetGroup}})
+
     app.logger.info(targetUser)
     app.logger.info(targetEvent)
+
     userId = targetUser.get("_id")
     myId = targetEvent.get("_id")
-    if myId != None :
-        mongo.db.events.update_one({'_id': myId}, {'$push': {'checkedInUsers': userId}})
-    return 'placeholder'
-
+    if myId != None:
+        event = mongo.db.events.find_one({'_id':myId})
+        if userId not in event['checkedInUsers']:
+            mongo.db.events.update_one({'_id': myId}, {'$push': {'checkedInUsers': userId}})
+            return "Successfully checked in"
+        else:
+            return "Looks like you're already checked in!"
 
 @app.route("/userGroups", methods=['POST'])
 def getAllGroupsForUser():
@@ -123,9 +154,39 @@ def getAllGroupsForUser():
     groups = targetUser.get('groups')
     return dumps(groups)
 
+@app.route("/exportToSlack", methods=['POST'])
+def slackExport():
+    groupName = request.args.get('group')
+    group = mongo.db.groups.find_one({"name": groupName})
+    users = group.get('users')
+
+    team = slack_client.api_call("users.list")
+    app.logger.debug(team)
+    teamEmails = ()
+    for user in team:
+        try:
+            app.logger.debug(user)
+            email = user.get('profile').get('email')
+            teamEmails.add(email)
+        except:
+            pass
+
+    emails =()
+    for user in users:
+        u = mongo.db.users.find_one({"_id": user})
+        email = u.get('email')
+        if email not in teamEmails:
+            callstring = "users.admin.invite?email={}".format(email)
+            app.logger.info(callstring)
+            slack_client.api_call(callstring)
+
+
+    return "invites sent"
 
 # LAUNCH APP
 if __name__ == "__main__":
         port = int(os.environ.get("PORT", 5000))
         app.logger.info("Getting Flask up and running...\n")
+        if slack_client.api_call("api.test") is not None:
+            app.logger.info("Connected to Slack!")
         app.run(host = '127.0.0.1' , port = port)
