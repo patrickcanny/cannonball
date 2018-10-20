63294f6a3be4dca65df2b1d85965b9c56f2e256e
Varun Chadha <vchadha023@gmail.com>
2018-10-20 02:17:33 -0500

    Added base file to submit new user to db

--- AndroidApp/cannonball_app/lib/main.dart
+++ AndroidApp/cannonball_app/lib/main.dart
@@ -1,249 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() => runApp(UserCreate());
 
-class table extends StatefulWidget {
-  const table({ Key key }) : super(key: key);
-
+class CreateUserForm extends StatefulWidget {
   @override
-  _Table createState() => _Table();
-}
-
-
-class _Table extends State<table>{
-
-  Widget bodyData() => DataTable(
-      onSelectAll: (b) {},
-      sortColumnIndex: 1,
-      sortAscending: true,
-      columns: <DataColumn>[
-        DataColumn(
-          label: Text("First Name"),
-          numeric: false,
-          tooltip: "To display first name of the Name",
-        ),
-        DataColumn(
-          label: Text("Last Name"),
-          numeric: false,
-          tooltip: "To display last name of the Name",
-        ),
-      ],
-      rows: names
-          .map(
-            (name) => DataRow(
-          cells: [
-            DataCell(
-              Text(name.firstName),
-              showEditIcon: false,
-              placeholder: false,
-            ),
-            DataCell(
-              Text(name.lastName),
-              showEditIcon: false,
-              placeholder: false,
-            )
-          ],
-        ),
-      )
-          .toList());
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Episode 5 - Data Table"),
-      ),
-      body: Container(
-        child: bodyData(),
-      ),
-    );
+  CreateUserFormState createState() {
+    return CreateUserFormState();
   }
 }
 
-
-class Name {
-  String firstName;
-  String lastName;
-
-  Name({this.firstName, this.lastName});
-}
-
-var names = <Name>[
-  Name(firstName: "Pawan", lastName: "Kumar"),
-  Name(firstName: "Aakash", lastName: "Tewari"),
-  Name(firstName: "Rohan", lastName: "Singh"),
-];
-
-class TapboxA extends StatefulWidget {
-  TapboxA({Key key}) : super(key: key);
+class CreateUserFormState extends State<CreateUserForm> {
+  final _formKey = GlobalKey<FormState>();
+  final textController = TextEditingController();
 
   @override
-  _TapboxAState createState() => _TapboxAState();
-}
-
-class _TapboxAState extends State<TapboxA> {
-  bool _active = false;
-
-  void _handleTap() {
-    setState(() {
-      _active = !_active;
-    });
-  }
-
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        child: Center(
-          child: Text(
-            _active ? 'Active' : 'Inactive',
-            style: TextStyle(fontSize: 32.0, color: Colors.white),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: textController,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+            },
           ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                  var url = "https://cannonball-220004.appspot.com/newUser?name=" + textController.text;
+                  http.get(url);
+                  return showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        // Retrieve the text the user has typed in using our
+                        // TextEditingController
+                        content: Text(textController.text),
+                      );
+                    },
+                  );
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-//------------------------- MyApp ----------------------------------
-
-
-class MyApp extends StatelessWidget {
+class UserCreate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+//    var url = "https://cannonball-220004.appspot.com/newUser";
+//    http.post(url, body: {"name": "doodle"})
+//        .then((response) {
+//          print("Response status: ${response.statusCode}");
+//          print("Response body: ${response.body}");
+//        });
+//    print("testing lol");
     return MaterialApp(
-        title: 'Check In App',
+        title: 'Create New User',
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Check In App'),
+            title: Text('Create User'),
           ),
           body: Center(
-            child: table(),
+            child: CreateUserForm(),
           ),
-
         )
     );
   }
 }
-//    floatingActionButton: new FloatingActionButton(
-////        onPressed: _incrementCounter,
-////        tooltip: 'Increment',
-////        child: new Icon(Icons.add),
-////      ), // This trailing comma makes auto-formatting nicer for build methods.
-
-
-
-//import 'package:flutter/material.dart';
-//
-//void main() => runApp(new MyApp());
-//
-//class MyApp extends StatelessWidget {
-//  // This widget is the root of your application.
-//  @override
-//  Widget build(BuildContext context) {
-//    return new MaterialApp(
-//      title: 'Flutter Demo',
-//      theme: new ThemeData(
-//        // This is the theme of your application.
-//        //
-//        // Try running your application with "flutter run". You'll see the
-//        // application has a blue toolbar. Then, without quitting the app, try
-//        // changing the primarySwatch below to Colors.green and then invoke
-//        // "hot reload" (press "r" in the console where you ran "flutter run",
-//        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-//        // counter didn't reset back to zero; the application is not restarted.
-//        primarySwatch: Colors.blue,
-//      ),
-//      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-//    );
-//  }
-//}
-//
-//class MyHomePage extends StatefulWidget {
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//
-//  // This widget is the home page of your application. It is stateful, meaning
-//  // that it has a State object (defined below) that contains fields that affect
-//  // how it looks.
-//
-//  // This class is the configuration for the state. It holds the values (in this
-//  // case the title) provided by the parent (in this case the App widget) and
-//  // used by the build method of the State. Fields in a Widget subclass are
-//  // always marked "final".
-//
-//  final String title;
-//
-//  @override
-//  _MyHomePageState createState() => new _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  int _counter = 0;
-//
-//  void _incrementCounter() {
-//    setState(() {
-//      // This call to setState tells the Flutter framework that something has
-//      // changed in this State, which causes it to rerun the build method below
-//      // so that the display can reflect the updated values. If we changed
-//      // _counter without calling setState(), then the build method would not be
-//      // called again, and so nothing would appear to happen.
-//      _counter++;
-//    });
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    // This method is rerun every time setState is called, for instance as done
-//    // by the _incrementCounter method above.
-//    //
-//    // The Flutter framework has been optimized to make rerunning build methods
-//    // fast, so that you can just rebuild anything that needs updating rather
-//    // than having to individually change instances of widgets.
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        // Here we take the value from the MyHomePage object that was created by
-//        // the App.build method, and use it to set our appbar title.
-//        title: new Text(widget.title),
-//      ),
-//      body: new Center(
-//        // Center is a layout widget. It takes a single child and positions it
-//        // in the middle of the parent.
-//        child: new Column(
-//          // Column is also layout widget. It takes a list of children and
-//          // arranges them vertically. By default, it sizes itself to fit its
-//          // children horizontally, and tries to be as tall as its parent.
-//          //
-//          // Invoke "debug paint" (press "p" in the console where you ran
-//          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-//          // window in IntelliJ) to see the wireframe for each widget.
-//          //
-//          // Column has various properties to control how it sizes itself and
-//          // how it positions its children. Here we use mainAxisAlignment to
-//          // center the children vertically; the main axis here is the vertical
-//          // axis because Columns are vertical (the cross axis would be
-//          // horizontal).
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            new Text(
-//              'You have pushed the button this many times:',
-//            ),
-//            new Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
-//          ],
-//        ),
-//      ),
-//      floatingActionButton: new FloatingActionButton(
-//        onPressed: _incrementCounter,
-//        tooltip: 'Increment',
-//        child: new Icon(Icons.add),
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
-//    );
-//  }
-//}
