1df1e6bb1bfc008f6b5151e5cbefc3c3fc6fbfb9
Varun Chadha <vchadha023@gmail.com>
2018-10-21 04:14:36 -0500

    Updated spacing of code

--- AndroidApp/cannonball_app/lib/screens/home_page.dart
+++ AndroidApp/cannonball_app/lib/screens/home_page.dart
@@ -11,12 +11,10 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-
 var apiKey = "AIzaSyAAFXiekUVvfsAe1miEFxau1t-XG1oL5yg";
 Map<String, double> _currentLocation;
 Future<Position> position =  Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 var events;
-
 
 class MapsDemo extends StatelessWidget {
   MapsDemo(this.mapWidget, this.controller);
@@ -31,8 +29,6 @@ class MapsDemo extends StatelessWidget {
     events = json.decode(await(Requests.POST(coords.toJson(), "/getNearbyEvents")));
   }
 
-  
-  
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -45,10 +41,8 @@ class MapsDemo extends StatelessWidget {
             child: const Text('Update Current Location'),
             onPressed: () {
               retreiveEvents();
-              
-              
               controller.animateCamera(CameraUpdate.newCameraPosition(
-                 CameraPosition(
+                CameraPosition(
                   bearing: 270.0,
                   target: LatLng(_currentLocation["latitude"], _currentLocation["longitude"]),
                   tilt: 35.0,
@@ -130,8 +124,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   void slackExport(String group) async {
-      String query = "/exportToSlack?group="+group;
-      Requests.POST(null, query);
+    String query = "/exportToSlack?group="+group;
+    Requests.POST(null, query);
   }
 
   @override
@@ -165,93 +159,93 @@ class _HomePageState extends State<HomePage> {
         actions: <Widget>[
 
           IconButton(
-          icon: const Icon(Icons.my_location),
-          onPressed: () {
-            final location = LatLng(_currentLocation["latitude"], _currentLocation["longitude"]);
-            mapController.markers.clear();
-            mapController.addMarker(MarkerOptions(
-              position: location,
-            ));
-            mapController.animateCamera(
-              CameraUpdate.newLatLngZoom(
-                  location, 15.0),
-            );
-          },
-        ),
-      ],
+            icon: const Icon(Icons.my_location),
+            onPressed: () {
+              final location = LatLng(_currentLocation["latitude"], _currentLocation["longitude"]);
+              mapController.markers.clear();
+              mapController.addMarker(MarkerOptions(
+                position: location,
+              ));
+              mapController.animateCamera(
+                CameraUpdate.newLatLngZoom(
+                    location, 15.0),
+              );
+            },
+          ),
+        ],
         backgroundColor: Colors.green,
       ),
-    drawer: new Drawer( child: ListView(
+      drawer: new Drawer( child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-         UserAccountsDrawerHeader(accountName: new Text("Harrison Luo"),accountEmail: new Text("luo.harrison@yahoo.com"), /*currentAccountPicture: new Image.asset('varun'), */decoration: BoxDecoration(color: new Color.fromRGBO(36, 120, 65, 1.0),
-         ),),
+          UserAccountsDrawerHeader(accountName: new Text("Harrison Luo"),accountEmail: new Text("luo.harrison@yahoo.com"), /*currentAccountPicture: new Image.asset('varun'), */decoration: BoxDecoration(color: new Color.fromRGBO(36, 120, 65, 1.0),
+          ),),
 
-                 ListTile(
-                   title: new Text(
-                     "Create Your Group",
-                     style: TextStyle(
-                       fontSize: 20.0,
-                       fontWeight: FontWeight.w700,
-                       
-                     ),),
-                   onTap:() {
-                      PopUp.renderPopUp(context, 'Create Group', 'Group Name', 'newGroup');
-                   },
+          ListTile(
+            title: new Text(
+              "Create Your Group",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
 
-                 ),
+              ),),
+            onTap:() {
+              PopUp.renderPopUp(context, 'Create Group', 'Group Name', 'newGroup');
+            },
 
-        new Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text("\nYour Groups", style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.w700,
-        ),),
-          ],
-        ),
-         new Container(
+          ),
 
-           height: 600.0,
-           child: new ListView.builder(
-             itemCount: events == null ? 0 : events.length,
-             itemBuilder: (BuildContext context, int index) {
-               return Card(
-                 child: new ExpansionTile(
-                   title: new Text(
-                     "${events[index]}",
-                     style: TextStyle(
-                       fontSize: 20.0,
-                       fontWeight: FontWeight.w700,
-                     ),),
-                   children: <Widget>[
-                     new RaisedButton(
-                       padding: const EdgeInsets.all(8.0),
-                       textColor: Colors.white,
-                       color: Colors.blue,
-                       onPressed:(){ PopUp.renderPopUp(context, 'Create Event', 'Event Name', 'newEvent'); },
-                       child: new Text("Create Event"),
+          new Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text("\nYour Groups", style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+              ),),
+            ],
+          ),
+          new Container(
 
-                     ),
-                     new RaisedButton(
-                       padding: const EdgeInsets.all(8.0),
-                       textColor: Colors.white,
-                       color: Colors.green,
-                       child: new Text("Export Group to Slack"),
+            height: 600.0,
+            child: new ListView.builder(
+              itemCount: events == null ? 0 : events.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: new ExpansionTile(
+                    title: new Text(
+                      "${events[index]}",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                      ),),
+                    children: <Widget>[
+                      new RaisedButton(
+                        padding: const EdgeInsets.all(8.0),
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        onPressed:(){ PopUp.renderPopUp(context, 'Create Event', 'Event Name', 'newEvent'); },
+                        child: new Text("Create Event"),
+
+                      ),
+                      new RaisedButton(
+                        padding: const EdgeInsets.all(8.0),
+                        textColor: Colors.white,
+                        color: Colors.green,
+                        child: new Text("Export Group to Slack"),
                         onPressed:() {
-                                slackExport('slackTestGroup');
-                               },
-                     ),
-                   ],
-                 ),
-               );
-             },
-           ),
-         ),
-      ],
-    ),
-    ),
+                          slackExport('slackTestGroup');
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      ),
 
       body: new Padding(
         padding: const EdgeInsets.all(20.0),
@@ -282,7 +276,7 @@ class _HomePageState extends State<HomePage> {
                           color: Colors.blue,
                           onPressed:(){ checkIn(events[index], SessionController.currentUserId()); },
                           child: new Text("Check in to group"),
-                          
+
                         ),
                       ],
                     ),
